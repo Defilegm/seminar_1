870722797e98de3ac7a2c4cf2be724d37acd3cc9
Ruslan <mephistopooh45@gmail.com>
2023-02-13 15:29:49 +0600

    inputresult function added

---
+++ question_1.java
@@ -0,0 +1,37 @@
+// Написать программу вычисления n-ого треугольного числа.
+
+
+// 1.0 Ввод n треугольника
+// 1.1 Расчет n-треугольного числа
+// 1.2 Вывод треугольниого числа
+
+
+
+
+public class question_1 {
+    public static void main(String[] args) {
+        
+        int n = inputresult(6);
+        int result = calc(n);
+        print(n,result);
+
+
+
+    }
+    // 2.0 Ввод n треугольника
+    public static int inputresult(int n) {
+        return  n;
+    }
+    // 2.1 Расчет n-треугольного числа 
+    public static int calc(int n){
+        int result;
+        
+        result = n*(n+1)/2;
+        return result;
+    }
+    // 2.2 Вывод треугольниого числа
+    public static void  print(int n,int obj){
+        System.out.printf("Треугольное число %d: %d", n,obj);
+    }
+    
+}
